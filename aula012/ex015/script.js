20de46f729f62cb326e1cac3e5f732d95fc00a79
Guilherme Rodrigues <guilhermenex999@gmail.com>
2025-03-04 00:36:43 -0300

    ex19update

--- aula012/ex015/script.js
+++ aula012/ex015/script.js
@@ -3,7 +3,9 @@ function verificar() {
   var ano = data.getFullYear();
   var fano = document.getElementById("txtano");
   var res = document.getElementById("res");
-  var outro = document.getElementById('outro')
+  var outro = document.getElementById("outro");
+  var img = document.createElement("img");
+  img.setAttribute("id", "foto");
 
   if (fano.value <= 0 || fano.value >= ano) {
     window.alert("Verifique os dados e tente novamente");
@@ -11,16 +13,32 @@ function verificar() {
     var fsex = document.getElementsByName("radsex");
     var idade = ano - Number(fano.value);
     res.innerHTML = `Idade calculada: ${idade}`;
-    var genero= ''
-    if(fsex[0].checked){
-      genero = 'Homem'
-    }else if (fsex[1].cheked){
-      genero = 'Mulher'
-    }else {
-      var outro = window.prompt('Com qual gênero você se identifica?')
-      genero = outro
-      
+    var genero = "";
+    if (fsex[0].checked) {
+      genero = "Homem";
+
+      if (idade >= 0 && idade < 10) {
+        img.setAttribute("src", "imagens/meninobebe.jpg");
+      } else if (idade < 21) {
+        img.setAttribute("src", "imagens/homemjovem.jpg");
+      } else if (idade < 50) {
+        img.setAttribute("src", "imagens/homemadulto.jpg");
+      } else {
+        img.setAttribute("src", "imagens/homemidoso.jpg");
+      }
+    } else if (fsex[1]) {
+      genero = "Mulher";
+      if (idade >= 0 && idade < 10) {
+        img.setAttribute("src", "imagens/meninabebe.jpg");
+      } else if (idade < 21) {
+        img.setAttribute("src", "imagens/mulherjovem.jpg");
+      } else if (idade < 50) {
+        img.setAttribute("src", "imagens/mulheradulta.jpg");
+      } else {
+        img.setAttribute("src", "imagens/mulheridosa.jpg");
+      }
     }
-    res.innerHTML=`Detectamos ${genero} com ${idade} anos.`
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
+    res.appendChild(img);
   }
 }
