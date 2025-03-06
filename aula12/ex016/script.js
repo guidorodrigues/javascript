043507c76ccc74d32bfd9f976bb0d678d2840e27
Guilherme Rodrigues <guilhermenex999@gmail.com>
2025-03-06 01:04:18 -0300

    up

---
+++ aula12/ex016/script.js
@@ -0,0 +1,15 @@
+function cllicar() {
+  var ini = document.getElementById("txti");
+  var fim = document.getElementById("txtf");
+  var passo = document.getElementById("txtp");
+  var res = document.getElementById("res");
+
+  if (
+    ini.value.length == 0 ||
+    fim.value.length == 0 ||
+    passo.value.length == 0
+  ) {
+    window.alert("Apresenta falta de dados!");
+    
+  }
+}
