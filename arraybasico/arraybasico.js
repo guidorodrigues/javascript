5201f5156620881d35221711000b2d10dd79c855
Guilherme Rodrigues <guilhermenex999@gmail.com>
2025-03-12 00:20:34 -0300

    update
    Arrays básico em Javascript

---
+++ arraybasico/arraybasico.js
@@ -0,0 +1,23 @@
+const alunos = ['Luiz', 'Maria', 'João']
+console.log(alunos)
+
+
+/*
+alunos.push('eduardo') // adiciona no final crianco mais um indice, porem automaticamente.
+alunos.push('Luiza')
+alunos.push('Fábio')
+
+console.log(alunos.length) // mostra o tamanho da lista de alunos (arrays)
+
+alunos[alunos.length] = 'Luiza' // adiciona no final e cria mais um indice
+alunos[alunos.length] = 'Fábio'
+alunos[alunos.length] = 'Luana'
+console.log(alunos)
+
+alunos[3] = 'Luana' //altera o indice 3 para Luana, especificamente
+
+//console.log(alunos[0]) // mostra o elemnto do indice
+//console.log(alunos[1])
+//console.log(alunos[2])]
+*/
+
