9b3ece800c386cec2c64851ad9d50a0cb0d62cf8
Guilherme Rodrigues <guilhermenex999@gmail.com>
2025-03-13 21:37:32 -0300

    update

---
+++ Exercicios/ex002.js
@@ -0,0 +1,8 @@
+// calculadora de IMC
+const nome = 'Guido Rodrigues'
+const idade = 18
+const altura = 8.1
+const peso = 84 
+
+const resultado = 84 % (8.1)**2
+console.log(`${nome} tem ${idade} anos, pesa ${peso}kg, tem ${altura} de altura e seu IMC é ${resultado}`)
