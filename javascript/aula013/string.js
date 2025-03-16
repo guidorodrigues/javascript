009096cad678a350aa84c7c53314c63ef592ad4a
Guilherme Rodrigues <guilhermenex999@gmail.com>
2025-03-15 22:39:51 -0300

    update

---
+++ javascript/aula013/string.js
@@ -0,0 +1,22 @@
+//indices          01234567
+const umaString = 'Um texto'
+console.log(umaString.toUpperCase()) // deixa a string toda em maiúscula
+console.log(umaString.toLowerCase()) // deixa a string toda em minuscula
+
+
+
+
+/*
+
+console.log(umaString.indexOf('o', 3)) // 7 
+console.log(umaString.indexOf('Um', 3)) // -1
+console.log(umaString.lastIndexOf('T', 3)) // de trá para frebte
+
+console.log(umaString.search(/[a-z]/g)) // busca todas as letras minusculas
+console.log(umaString.replace(/Um/, 'Outra'))//troca uma string
+console.log(umaString.length)//conta qunatos caracteres tem na string
+
+console.log(umaString.concat(' em um lindo dia!')) // concatenando
+console.log(umaString + ' em um lindo dia!') // concatenando
+console.log(`${umaString} em um lindo dia!`) // template
+*/
