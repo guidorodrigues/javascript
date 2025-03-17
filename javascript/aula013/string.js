bc75e35519913d1c99482fea74421d2eeeb7004f
Guilherme Rodrigues <guilhermenex999@gmail.com>
2025-03-17 00:28:32 -0300

    update

--- javascript/aula013/string.js
+++ javascript/aula013/string.js
@@ -2,8 +2,7 @@
 const umaString = 'Um texto'
 console.log(umaString.toUpperCase()) // deixa a string toda em maiúscula
 console.log(umaString.toLowerCase()) // deixa a string toda em minuscula
-
-
+console.log(umaString.slice(-3))
 
 
 /*
@@ -11,6 +10,12 @@ console.log(umaString.toLowerCase()) // deixa a string toda em minuscula
 console.log(umaString.indexOf('o', 3)) // 7 
 console.log(umaString.indexOf('Um', 3)) // -1
 console.log(umaString.lastIndexOf('T', 3)) // de trá para frebte
+
+console.log(umaString.slice(-3))//pega uma palara especifica do seu texto 
+
+
+console.log(umaString.indexOf('a')) // procura o indice do primeiro "a"
+console.log(umaString.lastIndexOfindexOf('a'))// procura o indice do ultimo "a"
 
 console.log(umaString.search(/[a-z]/g)) // busca todas as letras minusculas
 console.log(umaString.replace(/Um/, 'Outra'))//troca uma string
