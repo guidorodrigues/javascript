ce267da52a4ea6b2d695307783b15c627297179d
Guilherme Rodrigues <guilhermenex999@gmail.com>
2025-03-19 02:15:05 -0300

    Update aula 009

---
+++ javascript/aula009/number.js
@@ -0,0 +1,34 @@
+let num1 = 0.7
+let num2 = 0.1
+
+num1 = ((num1 * 100)+(num2*100))/100 //0.8
+num1 = ((num1 * 100)+(num2*100))/100 //0.9
+num1 = ((num1 * 100)+(num2*100))/100 //1
+
+console.log(num1)
+console.log(Number.isInteger(num1))
+
+
+/*
+console.log(number1.toString() + number2)//typeof string
+
+    console.log(number1.toString(2))//retorna representação binaria da váriavael
+    number1 = number1.toString() //converte oara sitring
+    console.log(typeof number1) //mostra o tipo do elemento 
+
+            retornando um valor temporariamente com string. se algum elemento da conta for uma string ele não realiza a soma, mas sim comvatena os valores (junta) para ser mais especifico.
+
+    console.log(number1.toString(2))
+
+        
+            retorna representação binaria da váriavael
+
+    console.log(number1.toFixed(2))//arredonda o valor para 2 casas decimais 
+ 
+    console.log(Number.isNaN(temp)) //retorna se isso é ou não é um NaN (não é um número)
+    console.log(Number.isInteger(number1))//retorna se a variavel é inteira ou não 
+
+
+
+
+*/
