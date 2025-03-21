33567bc533f642766e8cdaec23ad672b1da9e0a5
Guilherme Rodrigues <guilhermenex999@gmail.com>
2025-03-21 02:39:20 -0300

    Update aula006

---
+++ javascript/aula10/number-math.js
@@ -0,0 +1,20 @@
+
+let num1 = 9.6
+const aleatorio = Math.PI
+console.log(aleatorio.toFixed(2))
+
+
+
+
+
+
+/*
+let num2 = Math.floor(num1) //arredondo para baixo
+let num2 = Math.ceil(num1)  //arredonda para cima
+let num2 = Math.round(num1) //arredonda para o número mais proxumo
+
+num2 = Math.max(1,3,7,4,1500)   //retorna o maior numero da lista
+num2 = Math.min(1,3,7,4,1500)   //retorna o maior numero da lista
+
+const aleatorio = Math.round(Math.random()*(20-1)+5)    //d20  retorna um numero aleatorio
+*/
