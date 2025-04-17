bc23ae77663367dbd946c20bc698c06c1e3ff45e
Guilherme Rodrigues <guilhermenex999@gmail.com>
2025-04-17 17:25:04 -0300

    updaet javascript-logica

---
+++ javascript-logica/aula01/operadores.js
@@ -0,0 +1,11 @@
+/*
+    Operadores de comparação
+    > maior que
+    > menor que
+    >= maior que ou igual a
+    <= menor que ou igual a
+    == igualdade (valor) *************não usa
+    === igualdade restrita (tipo)
+    != diferente (valor) *********não usa
+    !== difrente restrito (valor tipo)
+*/
