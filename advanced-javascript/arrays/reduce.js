b70646ef93efc0e2461cae844bc0b1643be6a989
Guilherme Rodrigues <guilhermenex999@gmail.com>
2025-05-11 23:12:09 -0300

    Corrige a lógica da função para encontrar a pessoa mais velha e organiza o código para melhor legibilidade

--- advanced-javascript/arrays/reduce.js
+++ advanced-javascript/arrays/reduce.js
@@ -7,4 +7,44 @@ const total = numeros.reduce((acumulador, valor, indice, array) => {
   acumulador = acumulador + valor;
   return acumulador;
 }, 0);
-console.log(total); 
+console.log(total);
+
+console.log("------------------");
+
+const pares = numeros.reduce((acumulador, valor) => {
+  if (valor % 2 === 0) acumulador.push(valor);
+  return acumulador;
+}, []);
+console.log(pares);
+
+// confuso, odeio minha vida
+
+console.log("------------------");
+
+const dobro = numeros.reduce((acumulador, valor) => {
+  if (valor % 2 === 0) {
+    acumulador += valor;
+  }
+  return acumulador;
+}, 0);
+console.log(dobro);
+
+console.log("------------------");
+
+// Retorne a pessoas mais velha
+const pessoas = [
+  { nome: "Luiz", idade: 62 },
+  { nome: "Maria", idade: 23 },
+  { nome: "Eduardo", idade: 55 },
+  { nome: "Leticia", idade: 19 },
+  { nome: "Rosana", idade: 32 },
+  { nome: "Wallace", idade: 47 },
+];
+
+const maisVelha = pessoas.reduce((acumulador, valor) => {
+  if (acumulador.idade > valor.idade) {
+    return acumulador;
+    return valor;
+  }
+});
+console.log(maisVelha);
