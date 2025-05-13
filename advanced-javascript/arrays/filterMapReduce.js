794ac792a45f21ca1c27d97445808082ca2fb285
Guilherme Rodrigues <guilhermenex999@gmail.com>
2025-05-12 21:44:57 -0300

    Corrige formatação e adiciona ponto e vírgula em expressões no arquivo filterMapReduce.js

--- advanced-javascript/arrays/filterMapReduce.js
+++ advanced-javascript/arrays/filterMapReduce.js
@@ -1,9 +1,11 @@
-// Retorne a soma do dobro de todos os pares 
+// Retorne a soma do dobro de todos os pares
 // -> filter pares
 // -> dobrar os valores
 // -> reduzir (somar tudo)
 
 const numeros = [5, 50, 80, 1, 2, 3, 4, 5, 40, 200];
-const pares = numeros.filter((valor) => valor % 2 === 0)
+const pares = numeros.filter((valor) => valor % 2 === 0);
 const soma = pares.reduce((acc, valor) => acc + valor * 2, 0);
-console.log(soma)
+console.log(soma);
+
+console.log("----------------------------------");
